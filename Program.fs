1ffd72b24af96ae9d05743f7e14ab5ec7222bcd4
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-03-26 02:44:08 -0400

    comments

--- Program.fs
+++ Program.fs
@@ -1,5 +1,4 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿// First mentoring session with Mathias
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
